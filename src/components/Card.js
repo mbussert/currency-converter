048aaac80ef0982a3df5fb23832e58b6a01e1348
Mike Bussert <mbussert@gmail.com>
2021-10-03 21:27:19 -0400

    Fixed select boxes and button

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,6 +7,7 @@ import SwapHorizIcon from "@mui/icons-material/SwapHoriz";
 import {
   Button,
   FormControl,
+  Grid,
   InputAdornment,
   InputLabel,
   MenuItem,
@@ -27,72 +28,86 @@ export default function RecipeReviewCard() {
   };
 
   return (
-    <Card sx={{ maxWidth: 1000, paddingBottom: 2 }}>
-      <CardHeader
-        title="Currency Converter"
-        subheader="React Group Learn Project"
-      />
-      <CardContent>
-        <FormControl sx={{ m: 1 }}>
-          <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-amount"
-            type="number"
-            value={values.amount}
-            placeholder="1.00"
-            onChange={handleChange("amount")}
-            startAdornment={<InputAdornment position="start">$</InputAdornment>}
-            label="Amount"
-          />
-        </FormControl>
-        <FormControl sx={{ m: 1 }}>
-          <InputLabel htmlFor="outlined-adornment-from">From</InputLabel>
-          <Select
-            id="outlined-adornment-from"
-            value={values.from}
-            placeholder="USD - US Dollar"
-            onChange={handleChange("from")}
-            startAdornment={
-              <InputAdornment position="start">Flag</InputAdornment>
-            }
-            label="From"
-          >
-            <MenuItem value="USD">USD - US Dollar</MenuItem>
-          </Select>
-        </FormControl>
-        <IconButton size="large" sx={{ marginTop: 1 }}>
-          <SwapHorizIcon fontSize="inherit" />
-        </IconButton>
-        <FormControl sx={{ m: 1 }}>
-          <InputLabel htmlFor="outlined-adornment-to">To</InputLabel>
-          <Select
-            id="outlined-adornment-to"
-            value={values.to}
-            placeholder="EUR - Euro"
-            onChange={handleChange("to")}
-            startAdornment={
-              <InputAdornment position="start">Flag</InputAdornment>
-            }
-            label="To"
-          >
-            <MenuItem value="EUR">EUR - Euro</MenuItem>
-          </Select>
-        </FormControl>
-      </CardContent>
-      <CardContent>
-        <FormControl sx={{ m: 1 }}>
-          <InputLabel htmlFor="outlined-adornment-result">Result</InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-result"
-            value={values.result}
-            onChange={handleChange("result")}
-            startAdornment={<InputAdornment position="start">$</InputAdornment>}
-            label="Result"
-            readOnly
-          />
-        </FormControl>
-        <Button variant="contained">Convert</Button>
-      </CardContent>
-    </Card>
+    <Grid container justifyContent="center" alignItems="center">
+      <Card sx={{ maxWidth: 900, paddingBottom: 2 }}>
+        <CardHeader
+          title="Currency Converter"
+          subheader="React Group Learn Project"
+        />
+        <CardContent>
+          <FormControl sx={{ m: 1 }}>
+            <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-amount"
+              type="number"
+              value={values.amount}
+              sx={{ width: 250 }}
+              placeholder="1.00"
+              onChange={handleChange("amount")}
+              startAdornment={
+                <InputAdornment position="start">$</InputAdornment>
+              }
+              label="Amount"
+            />
+          </FormControl>
+          <FormControl sx={{ m: 1 }}>
+            <InputLabel htmlFor="outlined-adornment-from">From</InputLabel>
+            <Select
+              id="outlined-adornment-from"
+              value={values.from}
+              onChange={handleChange("from")}
+              defaultValue="USD"
+              label="From"
+              sx={{ width: 250 }}
+            >
+              <MenuItem value="USD">USD - US Dollar</MenuItem>
+              <MenuItem value="EUR">EUR - Euro</MenuItem>
+            </Select>
+          </FormControl>
+          <IconButton size="large" sx={{ marginTop: 1 }}>
+            <SwapHorizIcon fontSize="inherit" />
+          </IconButton>
+          <FormControl sx={{ m: 1 }}>
+            <InputLabel htmlFor="outlined-adornment-to">To</InputLabel>
+            <Select
+              id="outlined-adornment-to"
+              value={values.to}
+              onChange={handleChange("to")}
+              label="To"
+              defaultValue="EUR"
+              sx={{ width: 250 }}
+            >
+              <MenuItem value="USD">USD - US Dollar</MenuItem>
+              <MenuItem value="EUR">EUR - Euro</MenuItem>
+            </Select>
+          </FormControl>
+        </CardContent>
+        <Grid container sx={{ m: 1 }} spacing={2}>
+          <Grid item>
+            <FormControl>
+              <InputLabel htmlFor="outlined-adornment-result">
+                Result
+              </InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-result"
+                value={values.result}
+                onChange={handleChange("result")}
+                startAdornment={
+                  <InputAdornment position="start">$</InputAdornment>
+                }
+                label="Result"
+                readOnly
+                sx={{ width: 250 }}
+              />
+            </FormControl>
+          </Grid>
+          <Grid item container xs>
+            <Button sx={{ width: 250 }} variant="contained">
+              Convert
+            </Button>
+          </Grid>
+        </Grid>
+      </Card>
+    </Grid>
   );
 }
